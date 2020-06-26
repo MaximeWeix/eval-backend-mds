75f06691fa7b2238ebb6a0cb9f1518a87a72fa7e
MaximeWeix <62374895+MaximeWeix@users.noreply.github.com>
2020-06-26 09:15:38 +0200

    Add files via upload

---
+++ mds.php
@@ -0,0 +1,12 @@
+<?php
+
+<p>Un formulaire avec différents types de champs</p>
+<form action="getform.php" method="get">
+  <label>Prénom : <input type="text"></label><br>
+  <label>Nom : <input type="text"></label><br>
+  <label>Ville : <input type="text"></label><br>
+  <label>Adresse email : <input type="email"></label><br>
+  <label>numéro de téléphone : <input type="tel"></label><br>
+  <input type="submit" value="Envoyer">
+</form>
+
